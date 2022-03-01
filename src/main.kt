638f036f26818c35910fd02052505401628f209a
caro-mugo <mugonycaroline@gmail.com>
2022-03-01 22:30:34 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,29 @@
+fun main() {
+    //question1
+var campus="Akirachix"
+    var result=(campus[0].toString()+campus[2]+campus[3])
+    println(result)
+
+    //question 2
+
+var name="caroline"
+    var age=20
+    var action= "Hi, my name is $name and I am $age years old."
+    println(action)
+
+    //question 3
+
+    var car="vitz"
+    var getLength=car.length
+    println(getLength)
+
+    //question 4
+
+    var fullName="nyambura"
+    if (fullName=="sarah") {
+        println("Thats me.")
+    } else {
+        println("I don’t know who that is")
+    }
+
+}
